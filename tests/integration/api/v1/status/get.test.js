35413935feaa14ed155995e28e685cdbdd7a68b2
ianasampaio <iana.sampaio06@gmail.com>
2024-05-17 15:10:41 -0300

    finish `api/v1/status` endpoint

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,13 @@
 test("GET to api/v1/status shoul return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
+
+  const respondeBody = await response.json();
+
+  const parsedUpdatedAt = new Date(respondeBody.updated_at).toISOString();
+  expect(respondeBody.updated_at).toEqual(parsedUpdatedAt);
+
+  expect(respondeBody.dependencies.database.version).toEqual("16.2");
+  expect(respondeBody.dependencies.database.max_connections).toEqual(100);
+  expect(respondeBody.dependencies.database.opened_connections).toEqual(1);
 });
